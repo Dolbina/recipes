1b0253cfe244522ca9351cbad5e972c261474a0a
Ann Dolbina <dolbinaann@gmail.com>
2023-06-12 18:02:36 +0300

    add card

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,37 +1,39 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet';
+import {RecipeReviewCard} from '../components/RecipeReviewCard/RecipeReviewCard'
 
-import { setIsLoading } from 'redux/contacts/selector';
+
+
+// import { setIsLoading } from 'redux/contacts/selector';
 import { GlobalStyle } from '../components/GlobalStyle';
-import { ContactForm } from 'components/ContactForm/ContactForm';
-import { Filter } from 'components/Filter/Filter';
-import { ContactList } from 'components/ContactList/ContactList';
+
 import { Layout } from 'components/Layout/Layout';
-import { fetchContacts } from 'redux/contacts/operations';
+import { Grid } from '@mui/material';
 
-export default function Contacts() {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(setIsLoading);
 
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
 
+export default function Contacts (){
   return (
     <>
       <Helmet>
-        <title>Your contacts</title>
+        <title>Your recipes</title>
       </Helmet>
       <Layout>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <h2>Contacts</h2>
-        <Filter />
-        <div>
-          <div>{isLoading && 'Request in progress...'}</div>
-          <ContactList />
-        </div>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4} lg={3}>
+            <RecipeReviewCard />
+          </Grid>
+          <Grid item xs={12} md={4} lg={3}>
+            <RecipeReviewCard />
+          </Grid>
+          <Grid item xs={12} md={4} lg={3}>
+            <RecipeReviewCard />
+          </Grid>
+          <Grid item xs={12} md={4} lg={3}>
+            <RecipeReviewCard />
+          </Grid>
+        </Grid>
         <GlobalStyle />
       </Layout>
     </>
